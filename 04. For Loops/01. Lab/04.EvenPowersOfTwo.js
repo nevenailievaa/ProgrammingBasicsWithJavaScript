565f12110375a1caed65d9197005ed4b5f61f2e7
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2025-03-27 16:27:43 +0200

    Delete 04. For Loops/01. Lab directory

--- 04. For Loops/01. Lab/04.EvenPowersOfTwo.js
+++
@@ -1,5 +0,0 @@
-function evenPowersOfTwo(n) {
-    for (let i = 0; i <= n; i += 2) {
-        console.log(Math.pow(2, i));
-    }
-}
--- 04. For Loops/01. Lab/07.SumOfNumbers.js
+++
@@ -1,9 +0,0 @@
-function sumOfNumbers(numbersAsText) {
-    let sum = 0;
-
-    for (let i = 0; i < numbersAsText.length; i++) {
-        sum += Number(numbersAsText[i]);
-    }
-    
-    console.log(`The sum of the digits is:${sum}`);
-}
